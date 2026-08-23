89b18b3ef212f6fce8bcb7a03775f73a489e2847
cryogenized-spec <cryogenized@gmail.com>
2026-08-23 14:57:01 +0200

    Remove legacy module 02

--- src/js/modules/02-hidden-consequence-variables.js
+++
@@ -1,5 +0,0 @@
-/* ================= HIDDEN CONSEQUENCE VARIABLES ================= */
-var FLAGS={truth:0,deception:0,mercy:0,cruelty:0,intimidation:0,reputation:0,
- mara:0,noble:0,jesterInf:0,jesterInstab:0,truthRevealed:false,escalated:false,passed:false,
- bluffOk:false,intimOk:false,intimPart:false,maraCalm:false};
-
