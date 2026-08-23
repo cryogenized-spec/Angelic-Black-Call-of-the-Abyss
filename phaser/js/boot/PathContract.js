505f7ff6c982dbd86c732247449f797d6d7f0a42
cryogenized-spec <cryogenized@gmail.com>
2026-08-23 12:41:37 +0200

    Fix Phaser asset path contract

---
+++ phaser/js/boot/PathContract.js
@@ -0,0 +1,10 @@
+/* Angelic Black — canonical Phaser asset path contract. */
+(function(){
+  'use strict';
+  window.ANGELIC_ASSETS = Object.freeze({
+    root: '../assets/',
+    art: '../assets/art/',
+    sprites: '../assets/sprites/',
+    icons: '../assets/icons/'
+  });
+})();
