e4ccfeb7b6006dc5ade89cab34ec4ad750b1fb96
cryogenized-spec <cryogenized@gmail.com>
2026-08-23 04:49:15 +0200

    Phaser M2 — configure Arcade Physics

--- phaser/js/config.js
+++ phaser/js/config.js
@@ -1,13 +1,13 @@
 /* Angelic Black — Phaser 4 foundation configuration. */
 (function(){
   'use strict';
-
   window.ANGELIC_PHASER_CONFIG = {
     width: 960,
     height: 540,
     aspect: 16/9,
     worldWidth: 2880,
     ground: 420,
-    version: 'phaser-4.2.1'
+    version: 'phaser-4.2.1',
+    gravityY: 0
   };
 })();
